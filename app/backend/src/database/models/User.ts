d9d54c4784a41faeecb967ed06c7591b34f6cc19
Camila Mandai <cayumandai@gmail.com>
2023-03-01 21:56:30 -0300

    feat: cria model e migration de users

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,43 @@
+import { Model, STRING, INTEGER } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  declare readonly id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  username: {
+    type: STRING(255),
+    allowNull: false,
+  },
+  role: {
+    type: STRING(255),
+    allowNull: false,
+  },
+  email: {
+    type: STRING(255),
+    allowNull: false,
+  },
+  password: {
+    type: STRING(255),
+    allowNull: false,
+  },
+}, {
+  // ... Outras configs
+  underscored: true,
+  sequelize: db,
+  // modelName: 'example',
+  timestamps: false,
+});
+
+export default User;
