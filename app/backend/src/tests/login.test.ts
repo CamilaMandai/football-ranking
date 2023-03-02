2ca952884e354cc6b65f2c4ed35d81c561772044
Camila Mandai <cayumandai@gmail.com>
2023-03-02 17:25:52 -0300

    fix: na rota GET de /login, redireciona para rota /matches

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -1,6 +1,7 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
 import { Model } from 'sequelize';
+import IUser from '../api/interfaces/IUser';
 
 // @ts-ignore
 import chaiHttp = require('chai-http');
@@ -23,6 +24,23 @@ describe('Testes das rotas de /login', () => {
       token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjU0NTI3MTg5fQ.XS_9AA82iNoiVaASi0NtJpqOQ_gHSHhxrpIdigiT-fc" // Aqui deve ser o token gerado pelo backend.
     }
     sinon.stub(LoginService, 'validateUser').resolves(token);
+    // const users = [
+    //   {
+    //     email: "admin@admin.com",
+    //     id: 1,
+    //     password: "$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW",
+    //     role: "admin",
+    //     username: "Admin",
+    //   },
+    //   {
+    //     email: "user@user.com",
+    //     id: 2,
+    //     password: "$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO",
+    //     role: "user",
+    //     username: "User",
+    //   }
+    // ] as IUser[];
+    // sinon.stub(Model, 'findAll').resolves(users));
     const user = {
       email: 'user@email.com',
       senha: 123456,
